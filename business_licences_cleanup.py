0de7289f0c07efc58136e9961b48e883a49d53ed
Jaleed Ur Rehman <jaleed12@me.com>
2019-11-06 20:04:49 -0800

    added one hot encoding and nan removal

--- business_licences_cleanup.py
+++ business_licences_cleanup.py
@@ -9,6 +9,8 @@ data = pd.read_csv("./datasets/business-licences.csv", sep=";")
 
 # Converting Geom to individual lat/long
 
+print("Converting Geom to lat/long...")
+
 geom_col = data.loc[:, "Geom"]
 latitudes = []
 longitudes = []
@@ -25,7 +27,22 @@ for item in geom_col:
 data["Latitude"] = latitudes
 data["Longitude"] = longitudes
 
+
+# Removing null entries
+non_null_cols = ["Latitude", "Longitude", "IssuedDate", "ExpiryDate", "ExtractDate", "BusinessType", "BusinessSubType", "UnitType", "LocalArea"]
+samples_to_remove = []
+
+for i in range(data.shape[0]):
+    cur_sample = data.iloc[i]
+    for j in range(len(non_null_cols)):
+        if type(cur_sample[non_null_cols[j]]) == float and math.isnan(cur_sample[non_null_cols[j]]):
+            samples_to_remove.append(i)
+
+data.drop(samples_to_remove, inplace=True)
+
 # Normalizing dated cols to year only
+print("Standardizing datetime/dates to year only...")
+
 date_cols = data[["IssuedDate", "ExtractDate", "ExpiryDate"]]
 updated_issued_dates = []
 updated_extract_dates = []
@@ -55,7 +72,15 @@ data["IssuedYear"] = updated_issued_dates
 data["ExpiryYear"] = updated_expiry_dates
 data["ExtractYear"] = updated_extract_dates
 
+# One Hot encoding categorical features
+print("One Hot encoding categorical features...")
+
+categorical_feats = ["BusinessType", "BusinessSubType", "UnitType", "LocalArea"]
+data = pd.get_dummies(data, columns=categorical_feats, prefix=categorical_feats)
+
 # Dropping extraneous cols
+print("Dropping unimportant cols...")
+
 data.drop(columns=["Geom", "BusinessName", "BusinessTradeName", "Unit", "House", "City", "Province", "Country", "PostalCode", "IssuedDate", "ExtractDate", "ExpiryDate"], inplace=True)
 
 
